4a3c3db8164278720993ab2d02decdd15f1c00eb
Pang <pangshan2002@gmail.com>
2024-12-09 18:48:16 +0800

    remove redundant code and unused imports

--- src/components/ToolBar/DataMenu/ExportNetworkToImage/SvgExportForm.tsx
+++ src/components/ToolBar/DataMenu/ExportNetworkToImage/SvgExportForm.tsx
@@ -3,7 +3,6 @@ import {
   Button,
   FormControlLabel,
   Checkbox,
-  TextField,
   DialogActions,
   Typography,
 } from '@mui/material'
@@ -12,7 +11,6 @@ import { ReactElement, useState } from 'react'
 import { saveAs } from 'file-saver'
 import { ExportImageFormatProps } from './ExportNetworkToImageMenuItem'
 import { useRendererFunctionStore } from '../../../../store/RendererFunctionStore'
-import { IdType } from '../../../../models/IdType'
 import { useUiStateStore } from '../../../../store/UiStateStore'
 import { useWorkspaceStore } from '../../../../store/WorkspaceStore'
 
@@ -20,21 +18,25 @@ export const SvgExportForm = (props: ExportImageFormatProps): ReactElement => {
   const [loading, setLoading] = useState(false)
   const [fullBg, setFullBg] = useState(true)
 
-  const activeNetworkId: IdType = useUiStateStore(
-    (state) => state.ui.activeNetworkView,
-  )
-  const currentNetworkId: IdType = useWorkspaceStore(
+  const activeNetworkId = useUiStateStore((state) => state.ui.activeNetworkView)
+  const currentNetworkId = useWorkspaceStore(
     (state) => state.workspace.currentNetworkId,
   )
 
-  const targetNetworkId: IdType =
-    activeNetworkId === undefined || activeNetworkId === ''
-      ? currentNetworkId
-      : activeNetworkId
+  const targetNetworkId = activeNetworkId || currentNetworkId
 
   const svgFunction = useRendererFunctionStore((state) =>
     state.getFunction('cyjs', 'exportSvg', targetNetworkId),
   )
+
+  const handleExport = async () => {
+    setLoading(true)
+    const result = await svgFunction?.(fullBg)
+    const blob = new Blob([result], { type: 'image/svg+xml' })
+    saveAs(blob, `${props.fileName}.svg`)
+    setLoading(false)
+    props.handleClose()
+  }
 
   return (
     <Box
@@ -63,17 +65,7 @@ export const SvgExportForm = (props: ExportImageFormatProps): ReactElement => {
         <Button color="error" onClick={props.handleClose}>
           Cancel
         </Button>
-        <Button
-          disabled={loading}
-          onClick={async () => {
-            setLoading(true)
-            const result = await svgFunction?.(fullBg)
-            const blob = new Blob([result], { type: 'image/svg+xml' })
-            saveAs(blob, `${props.fileName}.svg`)
-            setLoading(false)
-            props.handleClose()
-          }}
-        >
+        <Button disabled={loading} onClick={handleExport}>
           Confirm
         </Button>
       </DialogActions>
